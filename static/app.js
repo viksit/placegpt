80ac5f02c1b7b6cb1090ab5bef19c0cc36d58faf
Joshua Levy <joshua@cal.berkeley.edu>
2023-04-16 10:25:48 -0700

    Frontend fixes.

--- static/app.js
+++ static/app.js
@@ -3,11 +3,13 @@ const imgElement = document.createElement('img');
 imgElement.alt = 'Canvas';
 imageContainer.appendChild(imgElement);
 
-function updateImage() {
-  const url = '/canvas?_=' + Date.now();
+const chatContainer = document.getElementById('chat-container');
 
+function updateContent() {
+  // Update image
+  const imageUrl = '/canvas?_=' + Date.now();
   const tempImg = new Image();
-  tempImg.src = url;
+  tempImg.src = imageUrl;
   tempImg.onload = () => {
     imgElement.classList.add('fade-out');
     imgElement.classList.add('image-canvas');
@@ -16,8 +18,28 @@ function updateImage() {
       imgElement.classList.remove('fade-out');
     }, 300);
   };
+
+  // Update chat history
+  fetch('/history')
+    .then(response => response.json())
+    .then(chatHistory => {
+      const tempChatContainer = document.createElement('div');
+
+      // Render each chat message as a div within the temporary chat container
+      chatHistory.forEach(chatMessage => {
+        const chatMessageDiv = document.createElement('div');
+        chatMessageDiv.textContent = chatMessage;
+        tempChatContainer.appendChild(chatMessageDiv);
+      });
+
+      // Replace the chat container's contents with the new messages
+      chatContainer.innerHTML = tempChatContainer.innerHTML;
+    })
+    .catch(error => {
+      console.error('There was a problem fetching chat history:', error);
+    });
 }
 
-updateImage();
+updateContent();
 
-setInterval(updateImage, 1000);
+setInterval(updateContent, 1000); // Update both image and chat history every 1000 ms
